bf2ed404eb133363f47f6253ba827e13f1bc9438
aqualeaflife <aquaticleaflife@gmail.com>
2026-09-15 22:17:03 -0400

    Delete cache-cleaner.js

--- cache-cleaner.js
+++
@@ -1,2 +0,0 @@
-// PlantGroove v10 cache cleaner. Safe for saved PlantGroove data.
-(async()=>{try{if('serviceWorker'in navigator){const r=await navigator.serviceWorker.getRegistrations();await Promise.all(r.map(x=>x.unregister()));}if('caches'in window){const k=await caches.keys();await Promise.all(k.map(x=>caches.delete(x)));}}catch(e){console.warn('PlantGroove cache cleanup skipped:',e);}})();
